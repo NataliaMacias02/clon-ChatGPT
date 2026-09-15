c134e5743c2675361113e84d2c965d34bbbb4380
Naty Macias <st4441@utr.edu.mx>
2026-09-14 22:32:49 -0600

    Integración React Hook Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import './App.css'
+import { useForm } from "react-hook-form";
 
 function App() {
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <>
-      <h1 className="text-3xl font-bold">
-        Tailwind funciona
-      </h1>
-    </>
-  )
+    <form onSubmit={handleSubmit(onSubmit)}>
+
+      <input
+        type="text"
+        placeholder="Nombre"
+        {...register("nombre", {
+          required: "El nombre es obligatorio"
+        })}
+      />
+
+      {errors.nombre && <p>{errors.nombre.message}</p>}
+
+      <button type="submit">
+        Enviar
+      </button>
+
+    </form>
+  );
 }
 
-export default App
+export default App;
