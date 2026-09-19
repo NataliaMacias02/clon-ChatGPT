50318cbe49695bf63da20dbc9169e0213c214f76
Naty Macias <st4441@utr.edu.mx>
2026-09-18 23:10:18 -0600

    Integrar React Hook Form con validaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useForm } from "react-hook-form";
 
 function App() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
   const onSubmit = (data) => {
     console.log(data);
@@ -23,19 +23,27 @@ function App() {
       </section>
 
       <form onSubmit={handleSubmit(onSubmit)} className="p-4 border-t flex gap-2">
-        <input
-          type="text"
-          placeholder="Escribe tu mensaje..."
-          {...register("mensaje")}
-          className="flex-1 border rounded-lg p-2"
-        />
+        <div className="flex-1">
+          <input
+            type="text"
+            placeholder="Escribe tu mensaje..."
+            {...register("mensaje", {
+              required: "El mensaje no puede estar vacío"
+            })}
+            className="w-full border rounded-lg p-2"
+          />
 
-        <button
-          type="submit"
-          className="px-4 py-2 rounded-lg"
-        >
-          Enviar
-        </button>
+          {errors.mensaje && (
+            <p>{errors.mensaje.message}</p>
+          )}
+
+          <button
+            type="submit"
+            className="px-4 py-2 rounded-lg"
+          >
+            Enviar
+          </button>
+        </div>
       </form>
 
     </main>
